9e1d8a4b758f492506fd50b9e0594518f34b4b63
Hilal <61974812+H-2020@users.noreply.github.com>
2021-09-16 22:04:55 +0200

    Add files via upload

---
+++ Les003/src/Les006Task01.java
@@ -0,0 +1,67 @@
+public class Les006Task01 {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Nieuwe berichten 16");
+        System.out.println("Nieuwe berichten 15");
+        System.out.println("Nieuwe berichten 14");
+        System.out.println("Nieuwe berichten 12");
+        System.out.println("Nieuwe berichten 11");
+        System.out.println("Nieuwe berichten 13");
+        System.out.println("Nieuwe berichten 24");
+        System.out.println("Nieuwe berichten 34");
+        System.out.println("Nieuwe berichten 44");
+        System.out.println("Nieuwe berichten 50");
+        System.out.println("Nieuwe berichten 46");
+        System.out.println("Nieuwe berichten 47");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        System.out.println("Nieuwe berichten ");
+        // Definieer kleurconstanten
+        String TEXT_RESET = "\u001B[0m";
+        String TEXT_BLACK = "\u001B[30m";
+        String TEXT_RED = "\u001B[31m";
+        String TEXT_GREEN = "\u001B[32m";
+        String TEXT_YELLOW = "\u001B[33m";
+        String TEXT_BLUE = "\u001B[34m";
+        String TEXT_PURPLE = "\u001B[35m";
+        String TEXT_CYAN = "\u001B[36m";
+        String TEXT_WHITE = "\u001B[37m";
+
+
+
+        // Implementatie
+        System.out.println(TEXT_RED + "This text is red!" + TEXT_RESET);
+
+    }
+}
