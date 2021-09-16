648fca36d01d0c19e74fdf63c52d201a9472030f
Hilal <61974812+H-2020@users.noreply.github.com>
2021-09-16 22:19:41 +0200

    Add files via upload

---
+++ Les014/src/ComplexTypes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ComplexTypes {
+
+    public static void main(String[] args) {
+
+        //PRINT
+        String welcome = "Welcome to somewhere..!";
+        System.out.println(welcome);
+
+        //MEM:declare a text data
+
+        int n1;
+        int n2 = 20;
+        int n3 = 20;
+        int n4 = 30;
+        int n5 = 50;
+        double n6 = 60.60;
+        double n7 = 70.70;
+        double n8 = 80.80;
+
+        System.out.println("Number 1 is " + n2);
+
+
+    }
+}
